a744da72755c68716a1cc7b6d1d14eeb1c5b0d59
jacob andersen <jacobma87@hotmail.com>
2019-06-28 09:18:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/materialcalculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.materialcalculator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_layout);
+    }
+
+}
